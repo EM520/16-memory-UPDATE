1cd219d989467fb4d2850e69ae0b2470ecf48547
punchcode <punchcode@punchcodes-MacBook-Pro.local>
2021-02-08 11:02:22 -0500

    update

--- memory.js
+++ memory.js
@@ -1,74 +1,95 @@
 //create a new game screen
-window.addEventListener("click", e => {
-    if (e) { 
-     //document.body.innerHTML = "<h1 id='gameOn'>GAME ON!!!</h1>"
-      //document.body.innerHTML=`<div id='cardContainer'></div>`
-    }
-  })
-//shulffe card random
- const arrback = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9]
+const start = document.querySelector("#startScreen");
+const nav = document.querySelector("#nav");
+nav.classList.add("hidden");
 
- function shuffle(array) {
-    var currentIndex = array.length, tempValue, randomIndex;
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-      // And swap it with the current element.
-      tempValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = tempValue;
-    }
-    return array;
+window.addEventListener("click", (e) => {
+  if (e) {
+    start.classList.add("hidden");
+    nav.classList.remove("hidden");
   }
-let randomCards = shuffle(arrback)
-console.log(randomCards)
+});
+//shulffe card random
+const arrback = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9];
+
+function shuffle(array) {
+  var currentIndex = array.length,
+    tempValue,
+    randomIndex;
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    // And swap it with the current element.
+    tempValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = tempValue;
+  }
+  return array;
+}
+let randomCards = shuffle(arrback);
+console.log(randomCards);
 
 //create cardlist interface
-const cardHtml=randomCards.map(item=>`
+const cardHtml = randomCards.map(
+  (item) => `
 <div class='flip-card' data-target='${item}'>
 ${item}
     <div class="flip-card-inner" >
 
-        <div class="flip-card-front">
+        <div class="flip-card-front data-target='${item}">
         
         </div>
-        <div class="flip-card-back">
+        <div class="flip-card-back data-target='${item}">
         
         </div>  
     </div>
-</div>`)
-document.querySelector('#cardContainer').innerHTML = cardHtml.join('')
-
+</div>`
+);
+document.querySelector("#cardContainer").innerHTML = cardHtml.join("");
 
 //add animation to card when click it to flip
-const cardContainer=document.querySelector('#cardContainer').addEventListener('click',function(e){
-    e.preventDefault()
-    let currentFlipCard = e.target.dataset.target
-    console.log(currentFlipCard)
-})
+const cardContainer = document
+  .querySelector("#cardContainer")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    let currentFlipCard = e.target.dataset.target;
+    console.log(currentFlipCard);
+  });
 // set timer
-// let hour,minute,second
-//     hour=minute=second=0
-// function start()
-// {
-//   int=setInterval(timer,50)
-// }
-// function timer()
-// {
-//   if(second>=60)
-//   {
-//     second=0;
-//     minute=minute+1;
-//   }
-
-//   if(minute>=60)
-//   {
-//     minute=0;
-//     hour=hour+1;
-//   }
-//   document.querySelector('#timetext').value=hour+':'+minute +':'+second
-
-// }
-
+function start_timer() {
+  let timer = document.querySelector("#timer").innerHTML;
+  let arr = timer.split(":");
+  let h = 00;
+  let m = 05;
+  let s = 00;
+  if (s == 0) {
+    if (m == 0) {
+      if (h == 0) {
+        alert("Time Out!");
+        window.location.reload();
+        return;
+      }
+      h--;
+      m = 60;
+      if (h < 10) {
+        h = "0" + h;
+      }
+    }
+    m--;
+    m = 60;
+    if (m < 10) {
+      m = "0" + m;
+    }
+    s = 59;
+  } else {
+    s--;
+    if (s < 10) {
+      s = "0" + s;
+    }
+    document.querySelector("#timer").innerHTML = h + ":" + m + ":" + s;
+    setTimeout(start_timer, 1000);
+  }
+}
+start_timer();
